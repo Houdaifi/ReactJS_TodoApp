fcbc205232efd52b2015396029711ad2e8f47b6e
Houdaifa Amzil <houdaifaamzil@gmail.com>
2022-03-08 11:54:33 +0100

    Add drag reorder feature

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEdit, faClipboardCheck, faTrash, faPalette } from '@fortawesome/free-solid-svg-icons';
+import { faEdit, faClipboardCheck, faTrash, faPalette, faGripLines } from '@fortawesome/free-solid-svg-icons';
+import { OrderGroup, OrderItem, arrayMove } from 'react-draggable-order';
 
 const Task = (props) => {
     
@@ -24,30 +25,39 @@ const Task = (props) => {
     return (
         <div className="flex flex-col justify-center items-center max-h-96 overflow-y-auto">
             <div className="flex flex-col md:w-1/2 space-y-3">
-                {/* <Draggable> */}
+                <OrderGroup>
                     {tasks && tasks.map((task, index) => { 
                         return (
-                            <div className="flex justify-between items-center bg-white hover:bg-gray-50 border p-6 shadow" key={index}>
-                                <div className={task.completed ? 'line-through': null} >{task.name}</div>
-                                <div className="flex space-x-8">
-                                    <button className="text-emerald-500" onClick={() => ToggleCompleted(index)}>
-                                        <FontAwesomeIcon icon={faClipboardCheck} />
-                                    </button>
-                                    <button className="text-blue-500">
-                                        <FontAwesomeIcon icon={faEdit} />
-                                    </button>
-                                    <button className="text-slate-500">
-                                        <FontAwesomeIcon icon={faPalette} />
-                                    </button>
-                                    <button className="text-red-500" onClick={() => DeleteTask(index)}>
-                                        <FontAwesomeIcon icon={faTrash} />
-                                    </button>
+                            <OrderItem key={index} index={index} onMove={(to) => setItem_in_local_storage_and_render_tasks(arrayMove(tasks, index, to))}>
+                                <div className="flex justify-between items-center bg-white hover:bg-gray-50 border p-6 shadow" key={index}>
+                                    <OrderItem.Handle>
+                                        <div>
+                                            <button className="text-red-500">
+                                                <FontAwesomeIcon icon={faGripLines} />
+                                            </button>
+                                        </div>
+                                    </OrderItem.Handle>
+                                    <div className={task.completed ? 'line-through': null} >{task.name}</div>
+                                    <div className="flex space-x-8">
+                                        <button className="text-emerald-500" onClick={() => ToggleCompleted(index)}>
+                                            <FontAwesomeIcon icon={faClipboardCheck} />
+                                        </button>
+                                        <button className="text-blue-500">
+                                            <FontAwesomeIcon icon={faEdit} />
+                                        </button>
+                                        <button className="text-slate-500">
+                                            <FontAwesomeIcon icon={faPalette} />
+                                        </button>
+                                        <button className="text-red-500" onClick={() => DeleteTask(index)}>
+                                            <FontAwesomeIcon icon={faTrash} />
+                                        </button>
+                                    </div>
                                 </div>
-                            </div>
+                            </OrderItem>
                         );
                     }
                 )}
-                {/* </Draggable> */}
+                </OrderGroup>
             </div>
         </div>
      );
